afab5afe0957772c6fb9a86e14d0d22d92cf070e
Onur Özmen <onurozmen2001@gmail.com>
2024-08-16 01:30:00 +0300

    s

--- api/users.js
+++ api/users.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
           if (user) {
             res.status(200).json(user); // Kullanıcıyı döndür
           } else {
-            res.status(405).json({ error: 'User not found' });
+            res.status(404).json({ error: 'User not found' });
           }
         } else {
           // Tüm kullanıcıları döndür

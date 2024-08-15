b472429316f531c3e7f36144f8d0f981b52e718b
Onur Özmen <onurozmen2001@gmail.com>
2024-08-15 21:07:53 +0300

    Update generate-token.js

--- api/generate-token.js
+++ api/generate-token.js
@@ -1,48 +1,32 @@
-import express from 'express';
+import { MongoClient } from 'mongodb';
 import jwt from 'jsonwebtoken';
 
-// Token'ı oluşturup saklamak için gereken değişkenler
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 let token = null;
 let tokenExpiration = null;
+
 export default async function handler(req, res) {
-    const { method } = req;
-    if (method=== 'POST') {
-      try {
-        app.get('/api/generate-token', (req, res) => {
-            if (!token || new Date() > tokenExpiration) {
-            const payload = { userId: 123, username: 'exampleUser' };
-            token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-            tokenExpiration = new Date(new Date().getTime() + 3600 * 1000); // 1 saat geçerlilik süresi
-            }
-        
-            res.json({ token });
-        });
-      } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-      } finally {
-        await client.close();
+  if (req.method === 'POST') {
+    try {
+      // Token'ı sadece bir kez oluşturup 1 saat geçerli olacak şekilde ayarlıyoruz
+      if (!token || new Date() > tokenExpiration) {
+        const payload = { userId: 123, username: 'exampleUser' };
+        token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+        tokenExpiration = new Date(new Date().getTime() + 3600 * 1000); // 1 saat geçerlilik süresi
       }
-    } else if (req.method === 'GET') {
-      try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-          return res.status(401).json({ error: 'Authorization token required' });
-        }
-  
-        const decoded = await verifyToken(token);
-        await client.connect();
-        const database = client.db('Users');
-        const collection = database.collection('Users');
-        const user = await collection.findOne({ _id: decoded.id });
-  
-        res.status(200).json(user);
-      } catch (error) {
-        res.status(401).json({ error: error.message });
-      } finally {
-        await client.close();
-      }
-    } else {
-      res.setHeader('Allow', ['GET', 'POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+      await client.close();
     }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}

9610c16d257f857a310c94fc48caaafffbcb4dab
Onur Özmen <onurozmen2001@gmail.com>
2024-08-15 23:53:48 +0300

    S

--- src/App.tsx
+++ src/App.tsx
@@ -38,17 +38,17 @@ const App: React.FC = () => {
     1000000000// Lord
   ];
 
-  interface UserData{
-    id:number;
-    first_name:string;
-    username?:string;
-    language_code:string;
+  interface UserData {
+    id: number;
+    first_name: string;
+    username?: string;
+    language_code: string;
   }
-  useEffect(()=>{
+  useEffect(() => {
     const tgData = WebApp.initDataUnsafe.user;
     setUserData(tgData as UserData);
-    
-  },[])
+
+  }, [])
   const [loading, setLoading] = useState(true); // Loading state
   const [levelIndex, setLevelIndex] = useState(6);
   const [points, setPoints] = useState(22749365);
@@ -80,7 +80,7 @@ const App: React.FC = () => {
 
     return `${paddedHours}:${paddedMinutes}`;
   };
-  
+
   useEffect(() => {
     const updateCountdowns = () => {
       setDailyRewardTimeLeft(calculateTimeLeft(0));
@@ -146,7 +146,7 @@ const App: React.FC = () => {
     const interval = setInterval(() => {
       setPoints(prevPoints => prevPoints + pointsPerSecond);
     }, 1000);
-    
+
     return () => clearInterval(interval);
   }, [profitPerHour]);
 
@@ -188,138 +188,142 @@ const App: React.FC = () => {
 
     fetchUserData();
   }, [token]);
-  if(loading){
-    return("");
-}else{
-  return (
-    <div className="bg-black flex justify-center">
-      <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
-        <div className="px-4 z-10">
-          <div className="flex items-center space-x-2 pt-4">
-            <div className="p-1 rounded-lg bg-[#1d2025]">
-              <Hamster size={24} className="text-[#d4d4d4]" />
+  if (loading) {
+    return (
+    <div className="loading-screen">
+      <div className="spinner"></div>
+    </div>
+    );
+  } else {
+    return (
+      <div className="bg-black flex justify-center">
+        <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
+          <div className="px-4 z-10">
+            <div className="flex items-center space-x-2 pt-4">
+              <div className="p-1 rounded-lg bg-[#1d2025]">
+                <Hamster size={24} className="text-[#d4d4d4]" />
+              </div>
+              <div>
+                <p className="text-sm">{userData?.first_name} ({userData?.id})</p>
+                <p className="text-sm">{userData?.language_code} ({userData?.username})</p>
+              </div>
             </div>
-            <div>
-            <p className="text-sm">Sana aşığım karımmmm</p>
-            <p className="text-sm">{userData?.language_code} ({userData?.username})</p>
+            <div className="flex items-center justify-between space-x-4 mt-1">
+              <div className="flex items-center w-1/3">
+                <div className="w-full">
+                  <div className="flex justify-between">
+                    <p className="text-sm">{levelNames[levelIndex]}</p>
+                    <p className="text-sm">{levelIndex + 1} <span className="text-[#95908a]">/ {levelNames.length}</span></p>
+                  </div>
+                  <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
+                    <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
+                      <div className="progress-gradient h-2 rounded-full" style={{ width: `${calculateProgress()}%` }}></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64">
+                <img src={binanceLogo} alt="Exchange" className="w-8 h-8" />
+                <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
+                <div className="flex-1 text-center">
+                  <p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
+                  <div className="flex items-center justify-center space-x-1">
+                    <img src={dollarCoin} alt="Dollar Coin" className="w-[18px] h-[18px]" />
+                    <p className="text-sm">{formatProfitPerHour(profitPerHour)}</p>
+                    <Info size={20} className="text-[#43433b]" />
+                  </div>
+                </div>
+                <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
+                <Settings className="text-white" />
+              </div>
             </div>
           </div>
-          <div className="flex items-center justify-between space-x-4 mt-1">
-            <div className="flex items-center w-1/3">
-              <div className="w-full">
-                <div className="flex justify-between">
-                  <p className="text-sm">Sana aşığım karımmmm of çok fena hemdee</p>
-                  <p className="text-sm">{levelIndex + 1} <span className="text-[#95908a]">/ {levelNames.length}</span></p>
+
+          <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
+            <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
+              <div className="px-4 mt-6 flex justify-between gap-2">
+                <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                  <div className="dot"></div>
+                  <img src={dailyReward} alt="Daily Reward" className="mx-auto w-12 h-12" />
+                  <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
+                  <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyRewardTimeLeft}</p>
                 </div>
-                <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
-                  <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
-                    <div className="progress-gradient h-2 rounded-full" style={{ width: `${calculateProgress()}%` }}></div>
+                <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                  <div className="dot"></div>
+                  <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-12 h-12" />
+                  <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
+                  <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyCipherTimeLeft}</p>
+                </div>
+                <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
+                  <div className="dot"></div>
+                  <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-12 h-12" />
+                  <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
+                  <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
+                </div>
+              </div>
+
+              <div className="px-4 mt-4 flex justify-center">
+                <div className="px-4 py-2 flex items-center space-x-2">
+                  <img src={dollarCoin} alt="Dollar Coin" className="w-10 h-10" />
+                  <p className="text-4xl text-white">{points.toLocaleString()}</p>
+                </div>
+              </div>
+
+              <div className="px-4 mt-4 flex justify-center">
+                <div
+                  className="w-80 h-80 p-4 rounded-full circle-outer"
+                  onClick={handleCardClick}
+                >
+                  <div className="w-full h-full rounded-full circle-inner">
+                    <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64">
-              <img src={binanceLogo} alt="Exchange" className="w-8 h-8" />
-              <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
-              <div className="flex-1 text-center">
-                <p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
-                <div className="flex items-center justify-center space-x-1">
-                  <img src={dollarCoin} alt="Dollar Coin" className="w-[18px] h-[18px]" />
-                  <p className="text-sm">{formatProfitPerHour(profitPerHour)}</p>
-                  <Info size={20} className="text-[#43433b]" />
-                </div>
-              </div>
-              <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
-              <Settings className="text-white" />
-            </div>
           </div>
         </div>
 
-        <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
-          <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
-            <div className="px-4 mt-6 flex justify-between gap-2">
-              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
-                <div className="dot"></div>
-                <img src={dailyReward} alt="Daily Reward" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyRewardTimeLeft}</p>
-              </div>
-              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
-                <div className="dot"></div>
-                <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyCipherTimeLeft}</p>
-              </div>
-              <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
-                <div className="dot"></div>
-                <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
-              </div>
-            </div>
-
-            <div className="px-4 mt-4 flex justify-center">
-              <div className="px-4 py-2 flex items-center space-x-2">
-                <img src={dollarCoin} alt="Dollar Coin" className="w-10 h-10" />
-                <p className="text-4xl text-white">{points.toLocaleString()}</p>
-              </div>
-            </div>
-
-            <div className="px-4 mt-4 flex justify-center">
-              <div
-                className="w-80 h-80 p-4 rounded-full circle-outer"
-                onClick={handleCardClick}
-              >
-                <div className="w-full h-full rounded-full circle-inner">
-                  <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
-                </div>
-              </div>
-            </div>
+        {/* Bottom fixed div */}
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
+          <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
+            <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
+            <p className="mt-1">Exchange</p>
+          </div>
+          <div className="text-center text-[#85827d] w-1/5">
+            <Mine className="w-8 h-8 mx-auto" />
+            <p className="mt-1">Mine</p>
+          </div>
+          <div className="text-center text-[#85827d] w-1/5">
+            <Friends className="w-8 h-8 mx-auto" />
+            <p className="mt-1">Friends</p>
+          </div>
+          <div className="text-center text-[#85827d] w-1/5">
+            <Coins className="w-8 h-8 mx-auto" />
+            <p className="mt-1">Earn</p>
+          </div>
+          <div className="text-center text-[#85827d] w-1/5">
+            <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
+            <p className="mt-1">Airdrop</p>
           </div>
         </div>
-      </div>
 
-      {/* Bottom fixed div */}
-      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
-        <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
-          <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Exchange</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <Mine className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Mine</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <Friends className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Friends</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <Coins className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Earn</p>
-        </div>
-        <div className="text-center text-[#85827d] w-1/5">
-          <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Airdrop</p>
-        </div>
+        {clicks.map((click) => (
+          <div
+            key={click.id}
+            className="absolute text-5xl font-bold opacity-0 text-white pointer-events-none"
+            style={{
+              top: `${click.y - 42}px`,
+              left: `${click.x - 28}px`,
+              animation: `float 1s ease-out`
+            }}
+            onAnimationEnd={() => handleAnimationEnd(click.id)}
+          >
+            {pointsToAdd}
+          </div>
+        ))}
       </div>
-
-      {clicks.map((click) => (
-        <div
-          key={click.id}
-          className="absolute text-5xl font-bold opacity-0 text-white pointer-events-none"
-          style={{
-            top: `${click.y - 42}px`,
-            left: `${click.x - 28}px`,
-            animation: `float 1s ease-out`
-          }}
-          onAnimationEnd={() => handleAnimationEnd(click.id)}
-        >
-          {pointsToAdd}
-        </div>
-      ))}
-    </div>
-  );
-}
+    );
+  }
 };
 
 export default App;

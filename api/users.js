c9aeb4ae0d0675c75c617150917aeaaf827d0dd6
Onur Özmen <onurozmen2001@gmail.com>
2024-08-16 01:17:16 +0300

    Update users.js

--- api/users.js
+++ api/users.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
 
         if (ids) {
           // Belirli bir ID'ye göre kullanıcıyı bul
-          const user = await collection.findOne({ telegramID: ids });
+          const user = await collection.findOne({ telegramID: "7418158431" });
           
           if (user) {
             res.status(200).json(user); // Kullanıcıyı döndür

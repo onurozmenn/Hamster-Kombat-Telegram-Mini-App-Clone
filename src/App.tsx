2fe18a6145224ef9f3f256f5c095e176058854de
Onur Özmen <onurozmen2001@gmail.com>
2024-08-16 22:54:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -175,7 +175,7 @@ const App: React.FC = () => {
           const tempUser = userData;
           setUserData(response.data);
           console.log(response.data["first_name"]);
-          if(userData == response.data["first_name"]){
+          if(userData?.first_name == response.data["first_name"]){
             console.log(tempUser);
             console.log(userData);
             console.log("aynı!");

1883d905bfa8f6afe87a129cd5bd0ffc341403cf
Onur Özmen <onurozmen2001@gmail.com>
2024-08-17 20:12:59 +0300

    Update TeamCard.tsx

--- src/icons/TeamCard.tsx
+++ src/icons/TeamCard.tsx
@@ -7,7 +7,7 @@ type TeamCardProps = {
     level: string;
     totalProfit: string;
     isLocked?: boolean;
-    onClickEvent:  React.MouseEvent<HTMLDivElement>;
+    onClickEvent:  React.MouseEventHandler<HTMLButtonElement>;
 };
 export const TeamCard: React.FC<TeamCardProps> = ({ imageSrc, title, profitPerHour, level, totalProfit, isLocked = false, onClickEvent }) => (
     <div className="bg-[#292c34] rounded-lg p-4 w-full relative flex flex-col items-center">
@@ -31,7 +31,7 @@ export const TeamCard: React.FC<TeamCardProps> = ({ imageSrc, title, profitPerHo
                 <p className="text-gray-400">Saat başı kar</p>
                 <p className="text-yellow-400 font-bold">{profitPerHour} ₺</p>
                 <p className="text-gray-400 text-sm mt-2">lvl {level}</p>
-                <button onClick={onClickEvent} className="text-white rounded p-1 px-4 mt-1 bg-[#43433b99] font-bold">{totalProfit} ₺</button>
+                <button onClick={onClickEvent} className="hover:bg-[#5ee2f4] text-white rounded p-1 px-4 mt-1 bg-[#43433b99] font-bold">{totalProfit} ₺</button>
             </>
         )}
     </div>

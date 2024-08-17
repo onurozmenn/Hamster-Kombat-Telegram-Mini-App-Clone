b01ed2e790b5651da474493abfb9bbfe30f2fe43
Onur Özmen <onurozmen2001@gmail.com>
2024-08-17 18:38:40 +0300

    Update TeamCard.tsx

--- src/icons/TeamCard.tsx
+++ src/icons/TeamCard.tsx
@@ -11,12 +11,10 @@ type TeamCardProps = {
 export const TeamCard: React.FC<TeamCardProps> = ({ imageSrc, title, profitPerHour, level, totalProfit, isLocked = false }) => (
     <div className="bg-[#292c34] rounded-lg p-4 w-full relative flex flex-col items-center">
         {isLocked && (
-            <div>
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center rounded-lg">
-            <img src={lockImage} alt="Locked" className="w-12 h-12" />
-            </div>
-                <>
-                
+            <>
+                <div className="z-10 absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center rounded-lg">
+                    <img src={lockImage} alt="Locked" className="w-12 h-12" />
+                </div>
                 <img src={imageSrc} alt={title} className="w-16 h-16 mb-2" />
                 <h2 className="text-white text-lg font-bold">{title}</h2>
                 <p className="text-gray-400">Saat başı kar</p>
@@ -24,7 +22,6 @@ export const TeamCard: React.FC<TeamCardProps> = ({ imageSrc, title, profitPerHo
                 <p className="text-gray-400 text-sm mt-2">lvl {level}</p>
                 <p className="text-yellow-400 font-bold">{totalProfit} ₺</p>
             </>
-            </div>
         )}
         {!isLocked && (
             <>

7bbea0d5fdc19170100cf556093f01724c71c88e
Onur Özmen <onurozmen2001@gmail.com>
2024-08-19 00:10:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import './index.css'
 import NonMobileWarning from "./NonMobileWarning.tsx";
 import React from 'react';
 import { allowDesktop } from './constants.tsx';
+import WebApp from '@twa-dev/sdk'
 
 // Function to check if the user is on a mobile device
 function isMobileDevice() {
   const userAgent = navigator.userAgent || navigator.vendor;
   return /android|ipad|iphone|ipod/.test(userAgent.toLowerCase());
 }
-
+WebApp.disableVerticalSwipes;
+WebApp.enableClosingConfirmation;
 // Conditional rendering based on device type
 const RootComponent = (isMobileDevice() || allowDesktop )? <App /> : <NonMobileWarning />;
 

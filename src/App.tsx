71cf826980f5b89908698e6fa0796fecaf06e855
Onur Özmen <onurozmen2001@gmail.com>
2024-08-19 00:16:36 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,7 @@ const App: React.FC = () => {
 
     WebApp.disableVerticalSwipes();
     WebApp.enableClosingConfirmation();
+    WebApp.expand();
     setUserData(userDatas);
     setTelegramData(true);
     console.log("telegram data değişti");
